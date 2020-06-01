c37fdfc10b36847de73c09c00949d54477ff22cc
JRPino <pino3789@gmail.com>
2020-06-01 16:53:08 -0700

    Added get route for api/workouts

---
+++ routes/apiRoutes.js
@@ -0,0 +1,14 @@
+var db = require("../models");
+
+module.exports = function(app) {
+
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.find({})
+        .then(workout => {
+            res.json(workout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+};
